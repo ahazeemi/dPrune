ff4cf53e678ef2d13fa260aab4642c4dda5cd04d
Abdul Hameed Azeemi <abdulhameed.azeemi@hotmail.com>
2025-07-12 22:07:51 +0500

    Add kenlm download utils

--- dprune/__init__.py
+++ dprune/__init__.py
@@ -1,35 +1,27 @@
-"""
-dPrune: A lightweight, extensible Python library for data pruning with Hugging Face datasets and transformers.
+"""dPrune: A library for data pruning and dataset curation."""
 
-This library provides tools for data pruning through a modular framework of Scorers and Pruners.
-"""
+from dprune.base import Scorer, Pruner
+from dprune.scorers import *
+from dprune.pruners import *
+from dprune.pipeline import DataPruningPipeline
+from dprune.callbacks import *
+from dprune.utils import (
+    download_kenlm_model,
+    download_multiple_kenlm_models,
+    get_supported_languages,
+    get_kenlm_model_path,
+    SUPPORTED_LANGUAGES
+)
 
-from .base import Scorer, Pruner
-from .pipeline import PruningPipeline
-from .callbacks import ForgettingCallback
-
-# Import main scorer classes
-from .scorers.supervised import CrossEntropyScorer, ForgettingScorer
-from .scorers.unsupervised import KMeansCentroidDistanceScorer
-
-# Import main pruner classes
-from .pruners.selection import TopKPruner, BottomKPruner, StratifiedPruner, RandomPruner
-
-__version__ = "0.0.1"
+__version__ = "0.1.0"
 __all__ = [
-    # Base classes
     "Scorer",
-    "Pruner",
-    # Core components
-    "PruningPipeline",
-    "ForgettingCallback",
-    # Scorers
-    "CrossEntropyScorer",
-    "ForgettingScorer", 
-    "KMeansCentroidDistanceScorer",
-    # Pruners
-    "TopKPruner",
-    "BottomKPruner",
-    "StratifiedPruner",
-    "RandomPruner",
+    "Pruner", 
+    "DataPruningPipeline",
+    # Utilities
+    "download_kenlm_model",
+    "download_multiple_kenlm_models",
+    "get_supported_languages",
+    "get_kenlm_model_path",
+    "SUPPORTED_LANGUAGES"
 ]
