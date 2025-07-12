dc90544fda11a2ea85f6ba6683cac8017f0c70b4
Abdul Hameed Azeemi <abdulhameed.azeemi@hotmail.com>
2025-07-12 22:14:20 +0500

    Fix __init.___.py

--- dprune/__init__.py
+++ dprune/__init__.py
@@ -3,25 +3,13 @@
 from dprune.base import Scorer, Pruner
 from dprune.scorers import *
 from dprune.pruners import *
-from dprune.pipeline import DataPruningPipeline
+from dprune.pipeline import PruningPipeline
 from dprune.callbacks import *
-from dprune.utils import (
-    download_kenlm_model,
-    download_multiple_kenlm_models,
-    get_supported_languages,
-    get_kenlm_model_path,
-    SUPPORTED_LANGUAGES
-)
+from dprune.utils import *
 
 __version__ = "0.1.0"
 __all__ = [
     "Scorer",
     "Pruner", 
-    "DataPruningPipeline",
-    # Utilities
-    "download_kenlm_model",
-    "download_multiple_kenlm_models",
-    "get_supported_languages",
-    "get_kenlm_model_path",
-    "SUPPORTED_LANGUAGES"
+    "PruningPipeline",
 ]
